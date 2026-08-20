0d0bdac56483ce841af85cf7d73d293e9a819872
Paolo <paolo@example.com>
2026-08-20 15:32:57 +0200

    Initial Commit  of Banking Microservice

---
+++ Microservices/Banking/MicroRabbit.Banking.Api/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MicroRabbit.Banking.Api.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Microservices/Banking/MicroRabbit.Banking.Api/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MicroRabbit.Banking.Api.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
