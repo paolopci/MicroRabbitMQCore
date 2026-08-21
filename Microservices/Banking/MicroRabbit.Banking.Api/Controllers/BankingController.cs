85f8ae37a116a730d67cb79f8623a125f8754341
Paolo <paolo@example.com>
2026-08-21 15:54:17 +0200

    Partially completed Banking Microservice

---
+++ Microservices/Banking/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> GetAccounts()
+        {
+            return Ok(_accountService.GetAccounts().ToList());
+        }
+    }
+}
--- Microservices/Banking/MicroRabbit.Banking.Api/Program.cs
+++ Microservices/Banking/MicroRabbit.Banking.Api/Program.cs
@@ -1,13 +1,36 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbitMQ.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<BankingDbContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection")
+    )
+ );
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Banking Microservice",
+        Version = "v1"
+    });
+});
 
+DependencyContainer.RegisterServices(builder.Services);
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -15,11 +38,20 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MicroRabbit Banking API v1");
+    });
+}
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
+app.MapControllers();
 app.MapRazorPages()
    .WithStaticAssets();
 
