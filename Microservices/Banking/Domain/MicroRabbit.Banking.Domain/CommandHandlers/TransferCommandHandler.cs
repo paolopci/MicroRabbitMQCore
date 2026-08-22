b47699823ddd6650157656bbdf337eb126e7d7b5
Paolo <paolo@example.com>
2026-08-22 12:17:54 +0200

    Implemented Publish and SendCommand for Banking Microservice

---
+++ Microservices/Banking/Domain/MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
+using MicroRabbitMQ.Domain.Core.Bus;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers
+{
+    public class TransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus _bus;
+
+        public TransferCommandHandler(IEventBus eventBus)
+        {
+            _bus = eventBus;
+        }
+
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+            //Publish event to RabbitMQ
+            _bus.Publish(new TransferCreatedEvent(request.From, request.To, request.Amount));
+            return Task.FromResult(true);
+        }
+    }
+}
--- Microservices/Banking/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ Microservices/Banking/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,6 +1,6 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MicroRabbit.Banking.Api.Controllers
@@ -16,10 +16,18 @@ namespace MicroRabbit.Banking.Api.Controllers
             _accountService = accountService;
         }
 
+        // Get api/banking
         [HttpGet]
         public ActionResult<IEnumerable<Account>> GetAccounts()
         {
             return Ok(_accountService.GetAccounts().ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            _accountService.Transfer(accountTransfer);
+            return Ok(accountTransfer);
         }
     }
 }
